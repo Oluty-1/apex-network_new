e8de36f435ac6bc227155e91cde1bdd45d915488
Oluty-1 <tayoefunshile@gmail.com>
2024-09-15 22:35:34 +0100

    new entrypoint file

--- Docker-files/app/entrypoint.sh
+++ Docker-files/app/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 # Parse APP_CONFIG and export variables
-if [ ! -z "$APP_CONFIG" ]; then
-    eval $(echo $APP_CONFIG | jq -r 'to_entries | map("export " + .key + "=\"" + (.value | tostring) + "\"") | .[]')
-fi
+eval $(echo $APP_CONFIG | jq -r 'to_entries | map("export " + .key + "=\"" + (.value | tostring) + "\"") | .[]')
 
 # Execute the main application
 exec ./apex_network apex_network_api
+
+# # Parse APP_CONFIG and set environment variables
+# eval $(echo $APP_CONFIG | jq -r 'to_entries | .[] | "export \(.key)=\(.value)"')
+
+# # Run the main application
+# exec "$@"
