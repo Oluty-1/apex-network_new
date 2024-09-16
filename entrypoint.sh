7ecb35c41f1cf6c2519f985628dbc9c062c156ae
Oluty-1 <tayoefunshile@gmail.com>
2024-09-16 01:21:37 +0100

    change in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Parse APP_CONFIG and export variables
-eval $(echo $APP_CONFIG | jq -r 'to_entries | map("export " + .key + "=\"" + (.value | tostring) + "\"") | .[]')
+# eval $(echo $APP_CONFIG | jq -r 'to_entries | map("export " + .key + "=\"" + (.value | tostring) + "\"") | .[]')
+eval $(echo $APP_CONFIG | jq -r 'to_entries | .[] | "export \(.key)=\(.value)"')
 
 # Execute the main application
 exec ./apex_network apex_network_api
 
 # # Parse APP_CONFIG and set environment variables
-# eval $(echo $APP_CONFIG | jq -r 'to_entries | .[] | "export \(.key)=\(.value)"')
 
 # # Run the main application
 # exec "$@"
